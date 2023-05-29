b22a86da5131ca63eb159b5eda77b1c19317cea0
Justin Turner Arthur <justinarthur@gmail.com>
2023-05-29 13:26:28 -0500

    Start on 4fsc to BT.601, reusing the main resampling logic.

--- vsfieldkit/sampling.py
+++ vsfieldkit/sampling.py
@@ -2,7 +2,7 @@ import math
 from fractions import Fraction
 from typing import Optional, Union
 
-from vapoursynth import Error, VideoNode
+from vapoursynth import Error, VideoNode, core
 
 from vsfieldkit.types import FormatSpecifier, Resizer
 from vsfieldkit.util import convert_format_if_needed
@@ -70,15 +70,15 @@ PAL_4FSC_ACTIVE_WIDTH = PAL_4FSC * PAL_LINE_ACTIVE_TIME
 
 def resample_bt601_as_4fsc(
     clip: VideoNode,
-    kernel: Resizer,
+    kernel: Resizer = core.resize.Spline36,
     format: Optional[FormatSpecifier] = None,
-    output_pad_factor=8,
+    output_width_factor: Optional[int] = 8,
     **format_resize_args,
 ) -> VideoNode:
     """Takes a clip sampled from analog video according to the BT.601 spec
     and generates new video as if sampled at four times the color sub-carrier
-    frequency (4fSC) instead. Optionally will change the sample format in the
-    same transformation.
+    frequency (4fSC) of the theoretical original signal instead. Optionally
+    changes the sample format in the same transformation.
     """
     if clip.width != 720:
         raise Error('Only full active BT.601 width (720 samples) is supported')
@@ -88,22 +88,71 @@ def resample_bt601_as_4fsc(
         src_active_width = PAL_ACTIVE_BT601_SAMPLES
         target_active_width = PAL_4FSC_ACTIVE_WIDTH
         src_original_top = None
-        src_original_height = None
+        target_height = None
     elif clip.height == 480:
         src_active_width = NTSC_170M_ACTIVE_BT601_SAMPLES
         target_active_width = NTSC_4FSC_ACTIVE_WIDTH
         # Assume vertical SMPTE RP 202 crop of ST 170M NTSC for compression.
         # This is 5 lines below the 486i start.
         src_original_top = -5
-        src_original_height = 486
+        target_height = 486
     elif clip.height == 486:
         # Full height of SMPTE 170M active video
         src_active_width = NTSC_170M_ACTIVE_BT601_SAMPLES
         target_active_width = NTSC_4FSC_ACTIVE_WIDTH
         src_original_top = None
-        src_original_height = None
+        target_height = None
     else:
         raise Error('480i, 486i, or 576i height required for BT.601 source')
+
+    return _resample_digitized_active_regions(
+        clip,
+        src_active_width=src_active_width,
+        src_original_top=src_original_top,
+        target_height=target_height,
+        target_active_width=target_active_width,
+        output_width_factor=output_width_factor,
+        format=format,
+        kernel=kernel,
+    )
+
+
+def resample_4fsc_as_bt601(
+    clip: VideoNode,
+    target_height: Optional[int] = None,
+    format: Optional[FormatSpecifier] = None,
+    kernel: Resizer = core.resize.Spline36
+):
+    if target_height not in (None, 480, 486, 576):
+        raise Error('Target height must be 480, 486, or 576.')
+
+    if clip.height == 576:
+        # Full height of ITU BT.470 and BT.1700 PAL active video.
+        src_active_width = PAL_4FSC_ACTIVE_WIDTH
+        target_active_width = PAL_ACTIVE_BT601_SAMPLES
+    elif clip.height == 486:
+        # Full height of SMPTE 170M active video
+        src_active_width = NTSC_4FSC_ACTIVE_WIDTH
+        target_active_width = NTSC_170M_ACTIVE_BT601_SAMPLES
+        if target_height == 480:
+            # Assume wanting RP 202 crop.
+            clip = clip.std.Crop(top=5, bottom=1)
+    elif clip.height == 480:
+        src_active_width = NTSC_4FSC_ACTIVE_WIDTH
+        target_active_width = NTSC_170M_ACTIVE_BT601_SAMPLES
+        # Leave the crop as-is.
+    else:
+        raise Error('480i, 486i, or 576i height required for 4fSC source')
+
+    return _resample_digitized_active_regions(
+        clip,
+        src_active_width=src_active_width,
+        target_height=target_height,
+        target_width=720,
+        target_active_width=target_active_width,
+        format=format,
+        kernel=kernel,
+    )
 
 
 def _resample_digitized_active_regions(
@@ -111,28 +160,33 @@ def _resample_digitized_active_regions(
     src_active_width: Union[int, Fraction],
     target_active_width: Union[int, Fraction],
     kernel: Resizer,
+    src_original_top: Optional[int, Fraction] = None,
+    target_height: Optional[int] = None,
+    target_width: Optional[int] = None,
     format: Optional[FormatSpecifier] = None,
-    output_pad_factor: int = 8
+    output_width_factor: int = 8
 ) -> VideoNode:
     whole_target_width = math.ceil(target_active_width)
-    if whole_target_width % output_pad_factor == 0:
+    if (
+        (not target_width)
+        and (whole_target_width % output_width_factor == 0)
+    ):
         target_width = whole_target_width
-    else:
+    elif not target_width:
         target_width = (
-                whole_target_width
-                + (
-                        output_pad_factor
-                        - (whole_target_width % output_pad_factor)
-                )
+            whole_target_width
+            + (
+                output_width_factor
+                - (whole_target_width % output_width_factor)
+            )
         )
     target_horizontal_pad = target_width - target_active_width
     src_horizontal_pad = (
-            (src_active_width / target_active_width)
-            * target_horizontal_pad
+        (src_active_width / target_active_width)
+        * target_horizontal_pad
     )
     src_left_pad = src_horizontal_pad / 2
 
-    # BT.601 has built-in padding of the real active width.
     src_original_horizontal_pad = clip.width - src_active_width
     src_original_left_pad = src_original_horizontal_pad / 2
 
@@ -148,7 +202,7 @@ def _resample_digitized_active_regions(
         ),
         src_width=float(src_active_width + src_horizontal_pad),
         src_height=(
-            None if src_original_height is None
-            else float(src_original_height)
+            None if target_height is None
+            else float(target_height)
         )
     )
